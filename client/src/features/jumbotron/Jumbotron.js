f1084976ad8ce2fd1c6966f028f07605b4c4dd9f
k1te <kmiller343@gmail.com>
2021-05-13 11:53:42 -0500

    basic jumbotron/landing components

---
+++ client/src/features/jumbotron/Jumbotron.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div className="container-fluid jumbotron">
+      <h1>La Dolce Vita</h1>
+    </div>
+  );
+};
+
+export default Jumbotron;
