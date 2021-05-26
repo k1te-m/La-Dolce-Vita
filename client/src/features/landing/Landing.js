a61635180376df24703463fb890a4ae31b42e1b7
k1te <kmiller343@gmail.com>
2021-05-26 13:57:03 -0500

    useEffect to loadUser on refresh

--- client/src/features/landing/Landing.js
+++ client/src/features/landing/Landing.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Jumbotron from "./jumbotron/Jumbotron";
 import History from "./history/History";
 import Featured from "./featured/Featured";
 import Delivery from "./delivery/Delivery";
 import Contact from "./contact/Contact";
+import { useSelector, useDispatch } from "react-redux";
+import { selectAuth, loadUser } from "../auth/authSlice";
 
 const Landing = () => {
+  const auth = useSelector(selectAuth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!auth.user) {
+      dispatch(loadUser());
+    }
+  }, [auth.user, dispatch]);
+
   return (
     <>
       <Jumbotron />
