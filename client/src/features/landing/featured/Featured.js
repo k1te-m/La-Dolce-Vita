a37788b24f221a53881855f0a811dc68f53dac0e
k1te <kmiller343@gmail.com>
2021-05-26 10:45:15 -0500

    basic featured component

---
+++ client/src/features/landing/featured/Featured.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Featured = () => {
+  return (
+    <div className="container featured">
+      <h3>Featured Items</h3>
+    </div>
+  );
+};
+
+export default Featured;
