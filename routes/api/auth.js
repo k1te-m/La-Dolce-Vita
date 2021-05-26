898b8c486017b0cc18e3d69240d719e3b000cd68
k1te <kmiller343@gmail.com>
2021-05-26 13:57:53 -0500

    api routes

---
+++ routes/api/auth.js
@@ -0,0 +1,105 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("../../middleware/auth");
+const User = require("../../models/user");
+require("dotenv").config();
+
+const jwtENV = process.env.JWT_SECRET;
+
+// @route     GET api/auth/user
+// @desc      Get user data
+// @access    Private
+router.get("/user", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) throw Error("User does not exist");
+    res.json(user);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route POST api/auth/register
+// @desc Register user
+// @access Public
+router.post("/register", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  // Validation
+  if (!firstName || !lastName || !email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) throw Error("User already exists");
+
+    const salt = await bcrypt.genSalt(10);
+    if (!salt) throw Error("Something went wrong with bcrypt");
+
+    const hash = await bcrypt.hash(password, salt);
+    if (!hash) throw Error("Something went wrong with password hashing");
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hash,
+    });
+
+    const savedUser = await newUser.save();
+    if (!savedUser) throw Error("Something went wrong saving the user");
+
+    const token = jwt.sign({ id: savedUser._id }, jwtENV, { expiresIn: "1h" });
+
+    res.status(200).json({
+      token,
+      user: {
+        _id: savedUser.id,
+        firstName: savedUser.firstName,
+        lastName: savedUser.lastName,
+        email: savedUser.email,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
+
+// @route POST api/auth/login
+// @desc Login user
+// @access Public
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validation
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) throw Error("User does not exist");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw Error("Invalid credentials");
+
+    const token = jwt.sign({ id: user._id }, jwtENV, { expiresIn: "1h" });
+    if (!token) throw Error("Could not sign the token");
+
+    res.status(200).json({
+      token,
+      user: {
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
+
+module.exports = router;
