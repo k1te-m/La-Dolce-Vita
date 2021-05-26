3b2c492d714cc5aeafbcbb6cefb6f6a4675e7428
k1te <kmiller343@gmail.com>
2021-05-26 10:44:53 -0500

    imported components to landing

--- client/src/features/landing/Landing.js
+++ client/src/features/landing/Landing.js
@@ -1,10 +1,18 @@
 import React from "react";
-import Jumbotron from "../jumbotron/Jumbotron";
+import Jumbotron from "./jumbotron/Jumbotron";
+import History from "./history/History";
+import Featured from "./featured/Featured";
+import Delivery from "./delivery/Delivery";
+import Contact from "./contact/Contact";
 
 const Landing = () => {
   return (
     <>
       <Jumbotron />
+      <Featured />
+      <History />
+      <Delivery />
+      <Contact />
     </>
   );
 };
